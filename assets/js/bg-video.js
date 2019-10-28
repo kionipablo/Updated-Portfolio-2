ab37a46ee462e90e637d86c1332b52d2627db654
kionipablo <53058512+kionipablo@users.noreply.github.com>
2019-10-27 18:05:19 -0700

    Add files via upload

---
+++ assets/js/bg-video.js
@@ -0,0 +1,12 @@
+﻿(function ($) {
+    'use strict';
+
+    //Init plugin
+    $(function () {
+        $('#home').YTPlayer({
+            fitToBackground: true,
+            videoId: 'S4L8T2kFFck'
+        });
+    });
+
+}(jQuery));
